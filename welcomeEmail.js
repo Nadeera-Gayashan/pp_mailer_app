61001c4dfa0cbbe3ba3ccdbd5448c594328df638
Fraideron <jiafen24@gmail.com>
2023-01-25 21:09:30 +0200

    Update welcomeEmail.js
    changed the title

--- welcomeEmail.js
+++ welcomeEmail.js
@@ -1,6 +1,6 @@
 const generateEmailBody = () => {
   return `
-        <h1>Welcome to Our Site!</h1>
+        <h1>Welcome to Our Site! </h1>
         <p>Thank you for signing up for an account. We're excited to have you on board.</p>
         <p>If you have any questions or need assistance, please don't hesitate to reach out to our support team.</p>
         <p>Best,<br>The PPL Team</p>
